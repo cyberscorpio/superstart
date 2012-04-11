6c75e5a53e96409e1894a4bad12cbc572929aa14
cyberscorpio <cyberscorpio@gmail.com>
2012-04-11 17:27:49 +0800

    .

--- xpi/modules/ssSiteManager.js
+++ xpi/modules/ssSiteManager.js
@@ -18,9 +18,12 @@ Cu.import("resource://gre/modules/NetUtil.jsm");
 Cu.import("resource://gre/modules/FileUtils.jsm");
 
 	let that = this;
-	let logger = this.logger;
 
 	// utilities
+	function log(s) {
+		that.logger.logStringMessage(s);
+	}
+
 	function SHA1 (msg) {
 		function tohex(s) {
 			let hc = '0123456789ABCDEF';
@@ -45,6 +48,239 @@ Cu.import("resource://gre/modules/FileUtils.jsm");
 	}
 
 
+	function emptySite() {
+		return {
+			url: null,
+			title: null,
+			name: null,
+			snapshots: [null, null, null] // [top left, whole, customized]
+		};
+	}
+
+	let file = FileUtils.getFile('ProfD', ['superstart', 'sites.json']);
+	let column = 4, row = 2;
+	let imgWidth = 212, imgHeight = 132;
+
+	let imgLoading = 'images/loading.gif';
+	let imgNoSnapshot = 'images/no-image.png';
+	let favIcon = 'chrome://mozapps/skin/places/defaultFavicon.png';
+
+	let inLoading = false;
+	let sites = [];
+	function load() {
+		inLoading = true;
+		sites = [];
+		let i = 0, l = column * row;
+		for (; i < l; ++ i) {
+			sites.push(emptySite());
+		}
+		let retry = 0;
+		do {
+			if (!file.exists() || retry > 0) {
+				if (!file.exists()) {
+					file.create(Ci.nsIFile.NORMAL_FILE_TYPE, FileUtils.PERMS_FILE);
+				}
+				save();
+			}
+			try {
+				let tmp = that.jparse(that.fileGetContents(file));
+				if (tmp.length == 0) {
+					save();
+				} else {
+					sites = tmp;
+					let dirty = false;
+					for (i = 0, l = sites.length; i < l; ++ i) {
+						let ss = sites[i].snapshots;
+						if (ss[0] == imgLoading || ss[1] == imgLoading) {
+							dirty = true;
+							ss[0] = ss[1] = imgNoSnapshot;
+						}
+					}
+					if (dirty) {
+						save();
+					}
+					align();
+				}
+				break;
+			} catch (e) {
+				log(e);
+				retry ++;
+			}
+		} while (retry < 2);
+		inLoading = false;
+		that.fireEvent('sites-loaded', null);
+	}
+
+	function align() {
+		let count = sites.length;
+		if (count < column * row) {
+			count = column * row - count;
+		} else {
+			count = row - (count % column);
+			count = count == row ? 0 : count;
+		}
+		if (count > 0) {
+			let idxes = [], l = sites.length;
+			for (let i = 0; i < count; ++ i) {
+				sites.push(emptySite());
+				idxes.push(l ++);
+			}
+
+			save();
+
+			if (!inLoading) {
+				that.fireEvent('sites-added', idxes);
+			}
+		}
+		// TODO: check empty lines (row)
+	}
+
+	function save() {
+		that.filePutContents(file, that.stringify(sites));
+	}
+
+	function get(idx1, idx2) {
+		if (idx1 >= 0 && idx1 < sites.length) {
+			let s = sites[idx1];
+			if (idx2 === undefined) {
+				return s;
+			} else {
+				if (Array.isArray(s) && idx2 >= 0 && idx2 < s.length) {
+					return s[idx2];
+				}
+			}
+		}
+
+		return null;
+	}
+
+	function decorateSite(s) {
+	}
+
+	// TODO: listen column / row changes
+
+	// methods
+	this.getSites = function() {
+		let rs = this.jparse(this.stringify(sites));
+		for (let i = 0, l = rs.length; i < l; ++ i) {
+			decorateSite(rs[i]);
+		}
+		return rs;
+	}
+
+	this.getSite = function(idx) {
+		let s = get(idx);
+		if (s == null) {
+			s = emptySite();
+		}
+		s = this.jparse(this.stringify(s));
+		decorateSite(s);
+		return s;
+	}
+
+
+	this.addSite = function(url, name, custImg) {
+		let i = 0, l = sites.length;
+		for (; i < l; ++ i) {
+			if (sites[i].url == null) {
+				break;
+			}
+		}
+		if (i == l) {
+			let idxes = [];
+			sites.push(emptySite());
+			idxes.push(l ++);
+
+			let count = sites.length;
+			if (count % column > 0) {
+				count = column - (count % column);
+				for (let j = 0; j < count; ++ j) {
+					sites.push(emptySite());
+					idxes.push(l ++);
+				}
+			}
+			save();
+			this.fireEvent('sites-added', idxes);
+		}
+
+		this.changeSite(i, url, name, custImg);
+	}
+
+	this.changeSite = function(idx, url, name, custImg) {
+		let s = get(idx);
+		if (s == null) {
+			return;
+		}
+
+		url = this.regulateUrl(url);
+		if (url == s.url) {
+			if (name != s.name || custImg != s.snapshots[2]) {
+				s.name = name;
+				s.snapshots[2] = custImg == '' ? null : custImg;
+				save();
+				this.fireEvent('sites-updated', [idx]);
+			}
+		} else {
+			this.removeSite(idx); // TODO: 1. if this site is the last one of a row, maybe cause problem...
+						// 2. idx should be idx1 & idx2
+			if (url == null) {
+				return;
+			}
+
+			s = get(idx);
+			s.url = s.title = url;
+			s.name = name;
+			s.icon = favIcon;
+			s.snapshots = [null, null, custImg != '' ? custImg : null];
+
+			this.refreshSite(idx);
+		}
+	}
+
+	this.exchangeSite = function(idx1, idx2) {
+	}
+
+	this.removeSite = function(idx) {
+	}
+
+	this.refreshSite = function(idx) {
+		let i = 0, l = sites.length;
+		if (idx == -1) {
+			for (; i < l; ++ i) {
+				this.refreshSite(i);
+			}
+			return;
+		}
+
+		let s = get(idx);
+		if (s == null || Array.isArray(s)) {
+			if (s != null) {
+				for (i = 0, l = s.length; i < l; ++ i) {
+					this.refreshSite(idx, i);
+				}
+			}
+			return;
+		}
+
+		if (s.snapshots[0] != null || s.snapshots[1] != null) {
+			s.snapshots = [imgLoading, imgLoading, s.snapshots[2]];
+			save();
+			this.fireEvent('sites-updated', [idx]);
+		}
+
+		fetchSite(idx);
+	}
+
+	// helper for methods
+	function doRefresh() {
+	}
+
+	////////////////////
+	// begin
+	load();
+
+
+	// snapshots
 	let sites2bFetched = [];
 	let fetchingCount = 0;
 	let fetchingMax = 5;
@@ -153,7 +389,7 @@ Cu.import("resource://gre/modules/FileUtils.jsm");
 			if (fetchingCount > 0) {
 				-- fetchingCount;
 			} else {
-				logger.logStringMessage('*** fetchingCount incorrect! ***');
+				log('*** fetchingCount incorrect! ***');
 			}
 			
 			if (sites2bFetched.length > 0) {
@@ -231,216 +467,6 @@ Cu.import("resource://gre/modules/FileUtils.jsm");
 		persist.saveURI(src, null, null, null, null, file);
 	}
 
-	function emptySite() {
-		return {
-			url: null,
-			title: null,
-			name: null,
-			snapshots: [null, null, null] // [top left, whole, customized]
-		};
-	}
-
-	let file = FileUtils.getFile('ProfD', ['superstart', 'sites.json']);
-	let column = 4, row = 2;
-	let imgWidth = 212, imgHeight = 132;
-
-	let imgLoading = 'images/loading.gif';
-	let imgNoSnapshot = 'images/no-image.png';
-	let favIcon = 'chrome://mozapps/skin/places/defaultFavicon.png';
-
-	let inLoading = false;
-	let sites = [];
-	function load() {
-		inLoading = true;
-		sites = [];
-		let i = 0, l = column * row;
-		for (; i < l; ++ i) {
-			sites.push(emptySite());
-		}
-		let retry = 0;
-		do {
-			if (!file.exists() || retry > 0) {
-				if (!file.exists()) {
-					file.create(Ci.nsIFile.NORMAL_FILE_TYPE, FileUtils.PERMS_FILE);
-				}
-				save();
-			}
-			try {
-				let tmp = that.jparse(that.fileGetContents(file));
-				if (tmp.length == 0) {
-					save();
-				} else {
-					sites = tmp;
-					let dirty = false;
-					for (i = 0, l = sites.length; i < l; ++ i) {
-						let ss = sites[i].snapshots;
-						if (ss[0] == imgLoading || ss[1] == imgLoading) {
-							dirty = true;
-							ss[0] = ss[1] = imgNoSnapshot;
-						}
-					}
-					if (dirty) {
-						save();
-					}
-					align();
-				}
-				break;
-			} catch (e) {
-				logger.logStringMessage(e);
-				retry ++;
-			}
-		} while (retry < 2);
-		inLoading = false;
-		that.fireEvent('sites-loaded', null);
-	}
-
-	function align() {
-		let count = sites.length;
-		if (count < column * row) {
-			count = column * row - count;
-		} else {
-			count = row - (count % column);
-			count = count == row ? 0 : count;
-		}
-		if (count > 0) {
-			let idxes = [], l = sites.length;
-			for (let i = 0; i < count; ++ i) {
-				sites.push(emptySite());
-				idxes.push(l ++);
-			}
-
-			save();
-
-			if (!inLoading) {
-				that.fireEvent('sites-added', idxes);
-			}
-		}
-		// TODO: check empty lines (row)
-	}
-
-	function save() {
-		that.filePutContents(file, that.stringify(sites));
-	}
-
-	function decorateSite(s) {
-	}
-
-	// TODO: listen column / row changes
-
-	// methods
-	this.getSites = function() {
-		let rs = this.jparse(this.stringify(sites));
-		for (let i = 0, l = rs.length; i < l; ++ i) {
-			decorateSite(rs[i]);
-		}
-		return rs;
-	}
-
-	this.getSite = function(idx) {
-		let s = emptySite();
-		if (idx >= 0 && idx < sites.length) {
-			s = sites[idx];
-		}
-		s = this.jparse(this.stringify(s));
-		decorateSite(s);
-		return s;
-	}
-
-
-	this.addSite = function(url, name, custImg) {
-		let i = 0, l = sites.length;
-		for (; i < l; ++ i) {
-			if (sites[i].url == null) {
-				break;
-			}
-		}
-		if (i == l) {
-			let idxes = [];
-			sites.push(emptySite());
-			idxes.push(l ++);
-
-			let count = sites.length;
-			if (count % column > 0) {
-				count = column - (count % column);
-				for (let j = 0; j < count; ++ j) {
-					sites.push(emptySite());
-					idxes.push(l ++);
-				}
-			}
-			save();
-			this.fireEvent('sites-added', idxes);
-		}
-
-		this.changeSite(i, url, name, custImg);
-	}
-
-	this.changeSite = function(idx, url, name, custImg) {
-		if (idx < 0 || idx >= sites.length) {
-			return;
-		}
-
-		url = this.regulateUrl(url);
-		let s = sites[idx];
-		if (url == site.url) {
-			if (name != s.name || custImg != s.snapshots[2]) {
-				s.name = name;
-				s.snapshots[2] = custImg == '' ? null : custImg;
-				save();
-				this.fireEvent('sites-updated', [idx]);
-			}
-		} else {
-			this.removeSite(idx); // TODO: if this site is the last one of a row, maybe cause problem...
-			if (url == null) {
-				return;
-			}
-
-			s = sites[idx];
-			s.url = s.title = url;
-			s.name = name;
-			s.icon = favIcon;
-			s.snapshots = [null, null, custImg != '' ? custImg : null];
-
-			this.refreshSite(idx);
-		}
-	}
-
-	this.exchangeSite = function(idx1, idx2) {
-	}
-
-	this.removeSite = function(idx) {
-	}
-
-	this.refreshSite = function(idx) {
-		let i = 0, l = sites.length;
-		if (idx == -1) {
-			for (; i < l; ++ i) {
-				this.refreshSite(i);
-			}
-			return;
-		}
-
-		if (idx < 0 || idx >= l) {
-			logger.logStringMessage('[superstart] refreshSite(' + idx + ') out of range (' + l + ')');
-			return;
-		}
-
-		let s = sites[idx];
-		if (s.url == null) {
-			return; // should only happen under refreshSite(-1)
-		}
-
-		if (s.snapshots[0] != null || s.snapshots[1] != null) {
-			s.snapshots = [imgLoading, imgLoading, s.snapshots[2]];
-			save();
-			this.fireEvent('sites-updated', [idx]);
-		}
-
-		fetchSite(idx);
-	}
-
-	////////////////////
-	// begin
-	load();
 }
 
 
